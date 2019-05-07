352f09d3fe0d20d37f40c4debe165398f4e70784
richelbilderbeek <richel@richelbilderbeek.nl>
2019-05-07 11:58:14 +0200

    Thanks @OCLint

--- main.cpp
+++ main.cpp
@@ -7,8 +7,12 @@
 namespace boost {
   namespace units {
     //1 states that the amount is in moles (of molecules, in this context)
-    typedef derived_dimension<boost::units::amount_base_dimension,1>::type sulfide_molecule_amount_dimension;
-    typedef derived_dimension<boost::units::amount_base_dimension,1>::type hydrogen_molecule_amount_dimension;
+    typedef derived_dimension<
+      boost::units::amount_base_dimension, 1
+    >::type sulfide_molecule_amount_dimension;
+    typedef derived_dimension<
+      boost::units::amount_base_dimension, 1
+    >::type hydrogen_molecule_amount_dimension;
   } // namespace units
 } // namespace boost
 
@@ -16,10 +20,14 @@ namespace boost {
 namespace boost {
   namespace units {
     namespace si {
-      typedef unit<sulfide_molecule_amount_dimension,si::system> sulfide_molecule_amount;
-      typedef unit<hydrogen_molecule_amount_dimension,si::system> hydrogen_molecule_amount;
-      BOOST_UNITS_STATIC_CONSTANT(sulfide_molecules_mol,sulfide_molecule_amount);
-      BOOST_UNITS_STATIC_CONSTANT(hydrogen_molecules_mol,hydrogen_molecule_amount);
+      typedef unit<
+        sulfide_molecule_amount_dimension, si::system
+      > sulfide_molecule_amount;
+      typedef unit<
+        hydrogen_molecule_amount_dimension, si::system
+      > hydrogen_molecule_amount;
+      BOOST_UNITS_STATIC_CONSTANT(sulfide_molecules_mol, sulfide_molecule_amount); //!OCLINT indeed long variable name, I keep it in for readability of this example code
+      BOOST_UNITS_STATIC_CONSTANT(hydrogen_molecules_mol, hydrogen_molecule_amount); //!OCLINT indeed long variable name, I keep it in for readability of this example code
     } // namespace si
   } // namespace units
 } //namespace boost
